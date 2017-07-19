a25258213434a1799f9e99049f2158672915db52
Arthur Brown <arthur.brown@email.wsu.edu>
2017-07-19 15:01:30 -0700

    Minor Modifications to Sources
    issue was that the xadc ip was not returning any data
    resolved by modifying the top file and xadc ip to use the same triggers
    as the zybo xadc

--- src/hdl/XADCdemo.v
+++ src/hdl/XADCdemo.v
@@ -35,52 +35,49 @@ module XADCdemo(
    wire [15:0] data;   
    reg [6:0] Address_in;     
 
-  
-   
    //xadc instantiation connect the eoc_out .den_in to get continuous conversion
 
-   xadc_wiz_0 xadc
-          (
-          .daddr_in(Address_in),            // Address bus for the dynamic reconfiguration port
-          .dclk_in(CLK100MHZ),             // Clock input for the dynamic reconfiguration port
-          .den_in(enable),              // Enable Signal for the dynamic reconfiguration port
-          .di_in(0),               // Input data bus for the dynamic reconfiguration port
-          .dwe_in(0),              // Write Enable for the dynamic reconfiguration port
-          .reset_in(0),            // Reset signal for the System Monitor control logic
-          .vauxp4(ck_an_p[0]),              // Auxiliary channel 0
-          .vauxn4(ck_an_n[0]),
-          .vauxp5(ck_an_p[1]),              // Auxiliary channel 5
-          .vauxn5(ck_an_n[1]),
-          .vauxp6(ck_an_p[2]),              // Auxiliary channel 5
-          .vauxn6(ck_an_n[2]),
-          .vauxp7(ck_an_p[3]),              // Auxiliary channel 6
-          .vauxn7(ck_an_n[3]),
-          .vauxp15(ck_an_p[4]),              // Auxiliary channel 7
-          .vauxn15(ck_an_n[4]),
-          .vauxp0(ck_an_p[5]),             // Auxiliary channel 12
-          .vauxn0(ck_an_n[5]),
-          .vauxp12(ck_an_p[6]),             // Auxiliary channel 13
-          .vauxn12(ck_an_n[6]),
-          .vauxp13(ck_an_p[7]),             // Auxiliary channel 14
-          .vauxn13(ck_an_n[7]),
-          .vauxp14(ck_an_p[8]),             // Auxiliary channel 15
-          .vauxn14(ck_an_n[8]),
-          .busy_out(),            // ADC Busy signal
-          .channel_out(),         // Channel Selection Outputs
-          .do_out(data),              // Output data bus for dynamic reconfiguration port
-          .drdy_out(ready),            // Data ready signal for the dynamic reconfiguration port
-          .eoc_out(enable),             // End of Conversion Signal
-//          .eos_out(),             // End of Sequence Signal
-          .alarm_out(),           // OR'ed output of all the Alarms    
-          .vp_in(vp_in),               // Dedicated Analog Input Pair
-          .vn_in(vn_in));                 
-         
+    xadc_wiz_0 xadc
+        (
+        .daddr_in(Address_in),            // Address bus for the dynamic reconfiguration port
+        .dclk_in(CLK100MHZ),             // Clock input for the dynamic reconfiguration port
+        .den_in(enable & |sw),              // Enable Signal for the dynamic reconfiguration port
+        .di_in(0),               // Input data bus for the dynamic reconfiguration port
+        .dwe_in(0),              // Write Enable for the dynamic reconfiguration port
+        .reset_in(0),            // Reset signal for the System Monitor control logic
+        .busy_out(),            // ADC Busy signal
+        .channel_out(),         // Channel Selection Outputs
+        .do_out(data),              // Output data bus for dynamic reconfiguration port
+        .eoc_out(enable),             // End of Conversion Signal
+        .eos_out(),             // End of Sequence Signal
+        .alarm_out(),           // OR'ed output of all the Alarms  
+        .drdy_out(ready),            // Data ready signal for the dynamic reconfiguration port
+        
+        .vp_in(vp_in),               // Dedicated Analog Input Pair
+        .vn_in(vn_in),
+        .vauxp4(ck_an_p[0]),              // Auxiliary channel 0
+        .vauxn4(ck_an_n[0]),
+        .vauxp5(ck_an_p[1]),              // Auxiliary channel 5
+        .vauxn5(ck_an_n[1]),
+        .vauxp6(ck_an_p[2]),              // Auxiliary channel 5
+        .vauxn6(ck_an_n[2]),
+        .vauxp7(ck_an_p[3]),              // Auxiliary channel 6
+        .vauxn7(ck_an_n[3]),
+        .vauxp15(ck_an_p[4]),              // Auxiliary channel 7
+        .vauxn15(ck_an_n[4]),
+        .vauxp0(ck_an_p[5]),             // Auxiliary channel 12
+        .vauxn0(ck_an_n[5]),
+        .vauxp12(ck_an_p[6]),             // Auxiliary channel 13
+        .vauxn12(ck_an_n[6]),
+        .vauxp13(ck_an_p[7]),             // Auxiliary channel 14
+        .vauxn13(ck_an_n[7]),
+        .vauxp14(ck_an_p[8]),             // Auxiliary channel 15
+        .vauxn14(ck_an_n[8])
+    );
     
       //led visual dmm              
-      always @( posedge(CLK100MHZ))
-      begin            
-        if(ready == 1'b1)
-        begin
+      always @(posedge ready)
+      begin
           case (data[15:13])
             1:  LED <= 8'b11;
             2:  LED <= 8'b111;
@@ -88,14 +85,13 @@ module XADCdemo(
             4:  LED <= 8'b11111;
             5:  LED <= 8'b111111;
             6:  LED <= 8'b1111111;
-            7:  LED <= 8'b11111111;                    
+            7:  LED <= 8'b11111111;
            default: LED <= 8'b0; 
            endcase
-        end           
-      end
+        end
            
       //switch driver to choose channel
-      always @(posedge(CLK100MHZ))
+      always @(negedge ready)
       begin
         case(sw)
         0: Address_in <= 8'h14; //A0
